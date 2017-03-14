c87fd70c7566cbcd696138b5e1f5e9b1898d00dc
Rejurhf <mziomek108@gmail.com>
2017-03-14 23:18:22 +0100

    Palindrome Done

--- lab1/palindrome/Palindrome.cpp
+++ lab1/palindrome/Palindrome.cpp
@@ -8,7 +8,7 @@ bool is_palindrome(std::string str){
     const char *characters = str.c_str();
     size_t size = str.size();
     int lim = int(size)/2;
-    for(int i = 0; i<=lim; ++i){
+    for(int i = 0; i<lim; ++i){
         if(characters[i] != characters[size-1-i]) return false;
     }
     return true;
