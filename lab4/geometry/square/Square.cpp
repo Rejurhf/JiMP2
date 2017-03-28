474a30bd3f066fb1e3ae54938915c5a2e8938d42
Rejurhf <mziomek108@gmail.com>
2017-03-28 23:34:25 +0200

    Square Test

---
+++ lab4/geometry/square/Square.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Rejurhf on 28.03.2017.
+//
+
+#include "Square.h"
---
+++ lab4/geometry/square/Square.h
@@ -0,0 +1,8 @@
+//
+// Created by Rejurhf on 28.03.2017.
+//
+
+#ifndef JIMP_EXERCISES_SQUARE_H
+#define JIMP_EXERCISES_SQUARE_H
+
+#endif //JIMP_EXERCISES_SQUARE_H
