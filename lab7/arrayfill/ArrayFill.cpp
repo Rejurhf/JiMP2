d6f9b9ec58c87d984711d4e50fb9838b6aefe997
Rejurhf <mziomek108@gmail.com>
2017-04-21 17:13:50 +0200

    IncrementalFill and RandomFill Done

--- lab7/arrayfill/ArrayFill.cpp
+++ lab7/arrayfill/ArrayFill.cpp
@@ -2,17 +2,22 @@
 // Created by Student on 20/04/17.
 //
 
-#include <vector>
 #include "ArrayFill.h"
 
 namespace arrays {
     void FillArray(int size, Filler &filler, std::vector<int> *v) {
         v->clear();
         v->reserve(size);
-        for (size_t i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             v->emplace_back(filler.fill(i));
         }
     }
 
-    int IncrementalFill::fill(int index) const {}
+    int IncrementalFill::fill(int index) {
+        return value_a_ * index + value_b_;
+    }
+
+    int RandomFill::fill(int index) {
+        return distributor_(generator_);
+    }
 }
--- lab7/arrayfill/ArrayFill.h
+++ lab7/arrayfill/ArrayFill.h
@@ -5,21 +5,36 @@
 #ifndef JIMP_EXERCISES_ARRAYFILL_H
 #define JIMP_EXERCISES_ARRAYFILL_H
 
+#include <vector>
+#include <random>
+
 namespace arrays {
     class Filler{
     public:
-        virtual int fill(int index) const = 0;
+        virtual int fill(int index) = 0;
     };
 
     class IncrementalFill : public Filler {
     public:
         IncrementalFill(int val):value_b_(val), value_a_(1){};
-        IncrementalFill(int val_a, int val_b):value_a_(val_a), value_b_(val_b){};
-        virtual int fill(int index) const override;
+        IncrementalFill(int val_a, int val_b):value_a_(val_b), value_b_(val_a){};
+        int fill(int index) override;
     private:
         int value_a_;
         int value_b_;
     };
+
+    class RandomFill : public Filler{
+    public:
+        RandomFill(std::default_random_engine generator, std::uniform_int_distribution<int> distributor):
+                generator_(move(generator)), distributor_(move(distributor)){};
+        int fill(int index) override;
+    private:
+        std::default_random_engine generator_;
+        std::uniform_int_distribution<int> distributor_;
+    };
+
+    void FillArray(int size, Filler &filler, std::vector<int> *v);
 }
 
 #endif //JIMP_EXERCISES_ARRAYFILL_H
