dfe523e22bbbfa033a6d15c022f909ea258fef95
Rejurhf <mziomek108@gmail.com>
2017-04-27 11:01:29 +0200

    Pessel In Progres

--- lab8/pesel/Pesel.cpp
+++ lab8/pesel/Pesel.cpp
@@ -2,14 +2,15 @@
 // Created by Student on 27/04/17.
 //
 
-#include <altivec.h>
+#include <regex>
 #include "Pesel.h"
-
 
 namespace academia {
     void Pesel::validatePESEL(const string pesel) {
         if(pesel.size() != 11)
             throw InvalidPeselLength();
-
+        std::regex pattern{R"((\d{11}))"};
+        string line{pesel};
+        std::smatch match;
     }
 }
