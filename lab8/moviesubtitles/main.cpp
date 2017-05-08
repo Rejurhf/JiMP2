ac711c39d56237cb82670daff4c146cefc5433bf
Rejurhf <mziomek108@gmail.com>
2017-05-08 20:04:17 +0200

    class XmlSerializer Problem

--- lab8/moviesubtitles/main.cpp
+++ lab8/moviesubtitles/main.cpp
@@ -11,6 +11,6 @@ int main(){
     MicroDvdSubtitles s;
     std::stringstream in {"{10}{200}COS TAM\n{260}{300}INNEGO\n"};
     std::stringstream out;
-    s.ShiftSubtitlesBy(&in, &out, 1400, 10);
+    s.ShiftAllSubtitlesBy(1400, 10, &in, &out);
     return 0;
 }
