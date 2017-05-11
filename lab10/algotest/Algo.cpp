3886edc1341e368a0c2fb4489d057551a8c72f9a
Rejurhf <mziomek108@gmail.com>
2017-05-11 10:16:40 +0200

    Algo3 In Progres

--- lab10/algotest/Algo.cpp
+++ lab10/algotest/Algo.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "Algo.h"
+
+namespace algo{
+    void CopyInto(const std::vector<int> &v, int n_elements, std::vector<int> *out){
+        std::copy_n(v.begin(), n_elements, std::back_inserter(*out));
+    }
+
+    bool Contains(const std::vector<int> &v, int element) {
+        return false;
+    }
+
+    void InitializeWith(int initial_value, std::vector<int> *v) {
+
+    }
+
+    std::vector<int> InitializedVectorOfLength(int length, int initial_value) {
+        return std::vector<int>();
+    }
+
+    std::vector<std::string> MapToString(const std::vector<double> &v) {
+        return std::vector<std::string>();
+    }
+}
--- lab10/algotest/Algo.h
+++ lab10/algotest/Algo.h
@@ -5,10 +5,14 @@
 #ifndef JIMP_EXERCISES_ALGO_H
 #define JIMP_EXERCISES_ALGO_H
 
+#include <vector>
 
-class Algo {
-
-};
-
+namespace algo {
+    void CopyInto(const std::vector<int> &v, int n_elements, std::vector<int> *out);
+    bool Contains(const std::vector<int> &v, int element);
+    void InitializeWith(int initial_value, std::vector<int> *v);
+    std::vector<int> InitializedVectorOfLength(int length, int initial_value);
+    std::vector<std::string> MapToString(const std::vector<double> &v);
+}
 
 #endif //JIMP_EXERCISES_ALGO_H
