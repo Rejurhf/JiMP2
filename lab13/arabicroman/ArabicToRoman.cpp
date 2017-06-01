4c58588adfc0a75133f6364d570dba36ffbf90ea
Rejurhf <mziomek108@gmail.com>
2017-06-01 11:02:54 +0200

    Conversion 1 to I Passed

--- lab13/arabicroman/ArabicToRoman.cpp
+++ lab13/arabicroman/ArabicToRoman.cpp
@@ -5,9 +5,25 @@
 #include "ArabicToRoman.h"
 
 namespace arabicroman{
+    void Times(string)
+
     string ConversionToRoman(int value) {
-        string number = "";
-        number += "I";
-        return number;
+        string roman = "";
+        int arabic = value, tmp;
+        while(arabic > 1){
+            tmp = 0;
+            if(arabic >= 1000){
+                tmp = arabic / 1000;
+                arabic -= tmp * 1000;
+                Times(roman, tmp);
+            }else if(arabic >= 900){
+                arabic -= 900;
+                roman += "CM";
+            }else if(arabic >= 500){
+                arabic -= 500;
+                arabic
+            }
+        }
+        return roman;
     }
 }
