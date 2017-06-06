a105979891a40888090b25aed86d3d08354c4183
Rejurhf <mziomek108@gmail.com>
2017-06-06 10:09:27 +0200

    Tree Done

--- lab12/tree/Tree.h
+++ lab12/tree/Tree.h
@@ -4,68 +4,149 @@
 
 #ifndef JIMP_EXERCISES_TREE_H
 #define JIMP_EXERCISES_TREE_H
+#include <memory>
+#include <iostream>
 
-#include <string>
-#include <queue>
-
-namespace tree{
-    class Leaf{
+namespace tree {
+    template<class T>
+    class Node{
     public:
-        Leaf(double value): value_(value), left_(nullptr), right_(nullptr){};
-        void SetLeftChild(Leaf &child){ left_ = &child; }
-        void SetRightChild(Leaf &child){ right_ = &child; }
-        double GetValue(){ return value_; }
-        void SetValue(double value){ value_ = value; }
-        Leaf GetLeftChild(){ return *left_; }
-        Leaf GetRightChild(){ return *right_; }
-    private:
-        double value_;
-        Leaf *left_, *right_;
+        Node(T in);
+
+        std::unique_ptr<Node<T>> left_;
+        std::unique_ptr<Node<T>> right_;
+
+        T value_;
     };
 
-    class Tree{
+    template<class T>
+    Node<T>::Node(T in) {
+        left_= nullptr;
+        right_= nullptr;
+        value_ = in;
+
+    }
+
+    template<class T>
+    class Tree {
     public:
-        Tree(): root_(nullptr) {};
-        Leaf GetRoot(){ return *root_; }
-        void AddLeaf(double value);
-        std::string PrintTree(Leaf tree);
+        Tree();
+        Tree(T in);
+
+        Node<T> * Root();
+        void Insert(T in);
+        std::size_t Size() const ;
+        std::size_t Depth() const ;
+        bool Find(const T &search, const std::unique_ptr<Node<T>> &root ) ;
+        T Remove(const T &search);
+        void Print(std::unique_ptr<Node<T>> &root ) const;
+        auto Value();
+
     private:
-        Leaf *root_;
+        std::size_t DFSDepth(const std::unique_ptr<Node<T>> &root) const;
+        std::unique_ptr<Node<T>> head_;
+        std::size_t size_;
     };
 
-    void Tree::AddLeaf(double value) {
-        Leaf child = Leaf(value);
-        Leaf *tmp;
-        std::queue<Leaf> childList;
-        if(root_ == nullptr)
-            *root_ = child;
+    template <class T>
+    Node<T> *Tree<T>::Root() {
+        return head_.get();
+    }
+
+    template<class T>
+    Tree<T>::Tree() {
+        size_=0;
+        Node<T> one( T() );
+        head_= nullptr;
+    }
+
+    template<class T>
+    Tree<T>::Tree(T in) {
+        head_ = std::make_unique<Node<T>>(in);
+        size_ = 1;
+    }
+
+    template<class T>
+    void Tree<T>::Insert(T in) {
+        if (head_.get()== nullptr){
+            head_ = std::make_unique<Node<T>>(in);
+            ++size_;
+        }
         else {
-            int position = 0;
-            tmp = root_;
-            while (position == 0) {
-                if (tmp->GetLeftChild() != nullptr and tmp->GetRightChild() != nullptr) {
-                    childList.push(tmp->GetLeftChild());
-                    childList.push(tmp->GetRightChild());
-                    tmp = &childList.front();
-                    childList.pop();
-                } else if (tmp->GetLeftChild() != nullptr)
-                    position = 1;
-                else
-                    position = 2;
+            auto *pivot = head_.get();
+            bool stop=false;
+            while (!stop) {
+                if (pivot->value_ < in) {
+
+                    if (pivot->right_ == nullptr) {
+                        pivot->right_ = std::make_unique<Node<T>>(in);
+                        stop = true;
+                        ++size_;
+                    }
+                    pivot = pivot->right_.get();
+
+
+                }
+                else {
+                    if (pivot->left_ == nullptr) {
+                        pivot->left_ = std::make_unique<Node<T>>(in);
+                        stop = true;
+                        ++size_;
+                    }
+                    pivot = pivot->left_.get();
+                }
             }
-            if(position == 1)
-                tmp->SetLeftChild(child);
-            else
-                tmp->SetRightChild(child);
         }
     }
 
-    std::string Tree::PrintTree(Leaf root){
-        Leaf tmp = root;
-        if(tmp.GetValue() == nullptr)
-            return "[none}";
-        return "[" + std::to_string(tmp.GetValue()) + " [" + PrintTree(tmp.GetLeftChild()) +
-                "] [" + PrintTree(tmp.GetRightChild()) + "]]";
+    template<class T>
+    bool Tree<T>::Find(const T &search, const std::unique_ptr<Node<T>> &root ) {
+
+        if (&root== nullptr)
+            return false;
+        if (&root->value_==search)
+            return true;
+        if (&root->value_ < search)
+            return this->Find(search, &root->left_);
+        else
+            return this->Find(search, &root->right_);
+    }
+
+    template<class T>
+    T Tree<T>::Remove(const T &search) {
+        --size_;
+        return nullptr;
+    }
+
+    template<class T>
+    void Tree<T>::Print(std::unique_ptr<Node<T>>& root) const {
+        std::cout<<root->value_;
+        if(root->left_!= nullptr)
+            Print(root->left_);
+        if(root->right_!= nullptr)
+            Print(root->right_);
+    }
+
+    template<class T>
+    auto Tree<T>::Value() {
+        return head_->value_;
+    }
+
+    template<class T>
+    std::size_t Tree<T>::Depth() const {
+        return DFSDepth(head_);
+    }
+
+    template<class T>
+    std::size_t Tree<T>::DFSDepth(const std::unique_ptr<Node<T>> &root) const {
+        if (root!= nullptr)
+            return 1 + std::max(this->DFSDepth(root->left_), this->DFSDepth(root->right_));
+        return 0;
+    }
+
+    template<class T>
+    std::size_t Tree<T>::Size() const {
+        return size_;
     }
 }
 #endif //JIMP_EXERCISES_TREE_H
--- lab12/tree/main.cpp
+++ lab12/tree/main.cpp
@@ -3,21 +3,17 @@
 //
 
 #include "Tree.h"
+#include <vector>
+#include <iostream>
 
+using namespace std;
 using namespace tree;
 
 int main(){
-    Tree myTree = Tree();
-    myTree.AddLeaf(0.1);
-    myTree.AddLeaf(1.2);
-    myTree.AddLeaf(2.3);
-    myTree.AddLeaf(3.4);
-    myTree.AddLeaf(4.5);
-    myTree.AddLeaf(5.6);
-    myTree.AddLeaf(6.7);
-    myTree.AddLeaf(7.8);
-    myTree.AddLeaf(8.9);
-    myTree.AddLeaf(9.0);
-    myTree.PrintTree(myTree.GetRoot());
+    Tree<int> first;
+
+    Node<int> node(5);
+    Node<int>(8);
+    std::make_unique<Node<int>>(9);
     return 0;
 }
