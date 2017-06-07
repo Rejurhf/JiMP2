73ccdd108f365acdf50a7b3decf969b88fa14a02
Rejurhf <mziomek108@gmail.com>
2017-06-07 20:08:46 +0200

    Roman to Arabic Done

--- lab13/arabicroman/ArabicToRoman.cpp
+++ lab13/arabicroman/ArabicToRoman.cpp
@@ -5,24 +5,51 @@
 #include "ArabicToRoman.h"
 
 namespace arabicroman{
-    void Times(string)
+    void TimesAddToRomanRemoveFromArabic(string &roman, string character, int &arabic, int number){
+        int tmp = 0;
+        tmp = arabic / number;
+        arabic -= tmp * number;
+        for(int i = 0; i < tmp; ++i)
+           roman += character;
+    }
+
+    void AddToRomanRemoveFromArabic(string &roman, string character, int &arabic, int number){
+        arabic -= number;
+        roman += character;
+    }
 
     string ConversionToRoman(int value) {
         string roman = "";
-        int arabic = value, tmp;
-        while(arabic > 1){
-            tmp = 0;
-            if(arabic >= 1000){
-                tmp = arabic / 1000;
-                arabic -= tmp * 1000;
-                Times(roman, tmp);
-            }else if(arabic >= 900){
-                arabic -= 900;
-                roman += "CM";
-            }else if(arabic >= 500){
-                arabic -= 500;
-                arabic
-            }
+        int arabic = value;
+        if(arabic > 3999)
+            return "error";
+        while(arabic > 0) {
+            if (arabic >= 1000)
+                TimesAddToRomanRemoveFromArabic(roman, "M", arabic, 1000);
+            else if (arabic >= 900)
+                AddToRomanRemoveFromArabic(roman, "CM", arabic, 900);
+            else if (arabic >= 500)
+                AddToRomanRemoveFromArabic(roman, "D", arabic, 500);
+            else if (arabic >= 400)
+                AddToRomanRemoveFromArabic(roman, "CD", arabic, 400);
+            else if (arabic >= 100)
+                TimesAddToRomanRemoveFromArabic(roman, "C", arabic, 100);
+            else if (arabic >= 90)
+                AddToRomanRemoveFromArabic(roman, "XC", arabic, 90);
+            else if (arabic >= 50)
+                AddToRomanRemoveFromArabic(roman, "L", arabic, 50);
+            else if (arabic >= 40)
+                AddToRomanRemoveFromArabic(roman, "XL", arabic, 40);
+            else if (arabic >= 10)
+                TimesAddToRomanRemoveFromArabic(roman, "X", arabic, 10);
+            else if (arabic == 9)
+                AddToRomanRemoveFromArabic(roman, "IX", arabic, 9);
+            else if (arabic >= 5)
+                AddToRomanRemoveFromArabic(roman, "V", arabic, 5);
+            else if (arabic == 4)
+                AddToRomanRemoveFromArabic(roman, "IV", arabic, 4);
+            else
+                TimesAddToRomanRemoveFromArabic(roman, "I", arabic, 1);
         }
         return roman;
     }
--- tests/lab13test/ArabicToRomanTest.cpp
+++ tests/lab13test/ArabicToRomanTest.cpp
@@ -11,10 +11,50 @@ class ArabicRomanTest: public ::testing::Test{
 public:
 };
 
-TEST_F(ArabicRomanTest, DoConvertOneToI){
+TEST_F(ArabicRomanTest, DoConvert1ToI){
     EXPECT_EQ("I", ConversionToRoman(1));
 }
 
-TEST_F(ArabicRomanTest, DoConvertTwoToII){
+TEST_F(ArabicRomanTest, DoConvert2ToII){
     EXPECT_EQ("II", ConversionToRoman(2));
+}
+
+TEST_F(ArabicRomanTest, DoConvert3ToIII){
+    EXPECT_EQ("III", ConversionToRoman(3));
+}
+
+TEST_F(ArabicRomanTest, DoConvert100ToC){
+    EXPECT_EQ("C", ConversionToRoman(100));
+}
+
+TEST_F(ArabicRomanTest, DoConvert626ToDCXXVI){
+    EXPECT_EQ("DCXXVI", ConversionToRoman(626));
+}
+
+TEST_F(ArabicRomanTest, DoConvert3798ToMMMDCCXCVIII){
+    EXPECT_EQ("MMMDCCXCVIII", ConversionToRoman(3798));
+}
+
+TEST_F(ArabicRomanTest, DoConvert888ToDCCCLXXXVIII){
+    EXPECT_EQ("DCCCLXXXVIII", ConversionToRoman(888));
+}
+
+TEST_F(ArabicRomanTest, DoConvert56ToLVI){
+    EXPECT_EQ("LVI", ConversionToRoman(56));
+}
+
+TEST_F(ArabicRomanTest, DoConvert1234ToMCCXXXIV){
+    EXPECT_EQ("MCCXXXIV", ConversionToRoman(1234));
+}
+
+TEST_F(ArabicRomanTest, DoConvert2017ToMMXVII){
+    EXPECT_EQ("MMXVII", ConversionToRoman(2017));
+}
+
+TEST_F(ArabicRomanTest, DoConvert987ToCMVXXXVII){
+    EXPECT_EQ("CMLXXXVII", ConversionToRoman(987));
+}
+
+TEST_F(ArabicRomanTest, DoConvert3333ToMMMCCCXXXIII){
+    EXPECT_EQ("MMMCCCXXXIII", ConversionToRoman(3333));
 }
